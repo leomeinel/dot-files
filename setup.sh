344aea6c9808514ea27d7c27f387925d0f735bea
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 12:22:47 +0200

    Disable kwallet

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 set -e
 cp -R ~/dot-files/Images ~/dot-files/.bashrc ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
 rm -Rf ~/dot-files
+{
+  echo "[Wallet]"
+  echo "Enabled=false"
+} > ~/.config/kwalletrc
