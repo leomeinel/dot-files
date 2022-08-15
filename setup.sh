9642800d8e2f4f1d69eec10ca0cf11d27576916d
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 13:04:45 +0200

    Move nvidia

--- setup.sh
+++ setup.sh
@@ -7,9 +7,4 @@ mkdir ~/.config
   echo "[Wallet]"
   echo "Enabled=false"
 } > ~/.config/kwalletrc
-if pacman -Qqd | grep -q "nvidia-settings"
-then
-nvidia-settings -r
-echo "nvidia-settings -l &" > ~/.xprofile
-fi
 rm -Rf ~/dot-files
