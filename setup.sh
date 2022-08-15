62ecb15bf889c20f2048567450e4cf5a2b192f0a
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 15:55:31 +0200

    Add comments

--- setup.sh
+++ setup.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
+# Fail on error
 set -e
+
+# Copy dot-files
 cp -R ~/dot-files/.bashrc ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
+
+# Copy nvidia-install.sh if required
 if pacman -Qq | grep -q "nvidia-settings"
 then
 echo "nvidia-settings -l &" > ~/.xprofile
 cp -R ~/dot-files/nvidia-install.sh ~/
 chmod +x ~/nvidia-install.sh
 fi
+
+# Disable KWallet
 mkdir ~/.config
 {
   echo "[Wallet]"
   echo "Enabled=false"
 } > ~/.config/kwalletrc
+
+# Remove repo
 rm -Rf ~/dot-files
