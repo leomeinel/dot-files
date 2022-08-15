c4ee6b3af5761092129cd85e531bee520f90db54
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 18:41:10 +0200

    Remove grep from pacman -Qq

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ set -e
 cp -R ~/dot-files/.bashrc ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
 
 # Copy nvidia-install.sh if required
-if pacman -Qq | grep -q "nvidia-settings"
+if pacman -Qq "nvidia-settings"
 then
 echo "nvidia-settings -l &" > ~/.xprofile
 cp -R ~/dot-files/nvidia-install.sh ~/
