baed2a9c9ff90d818434f127d2835d78910df1f9
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-17 22:18:45 +0200

    Reformat if..

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ set -e
 cp -R ~/dot-files/.bashrc ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
 
 # Copy nvidia-install.sh if required
-pacman -Qq "nvidia-settings" && echo "nvidia-settings -l &" > ~/.xprofile && cp -R ~/dot-files/nvidia-install.sh ~/ && chmod +x ~/nvidia-install.sh
+pacman -Qq "nvidia-settings" &&
+echo "nvidia-settings -l &" > ~/.xprofile &&
+cp -R ~/dot-files/nvidia-install.sh ~/ &&
+chmod +x ~/nvidia-install.sh
 
 # Disable KWallet
 mkdir ~/.config
