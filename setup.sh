cfe6631090c7f026d130db55ad4af5786b277c16
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-19 15:02:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,17 @@
 set -e
 
 # Copy dot-files
-cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
+cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.xprofile ~/dot-files/.gitconfig ~/dot-files/.Xresources ~/
 chmod 744 ~/.bashrc_aliases
 
 # Copy nvidia-install.sh if required
 pacman -Qq "nvidia-settings" &&
-echo "nvidia-settings -l &" > ~/.xprofile &&
-cp -R ~/dot-files/nvidia-install.sh ~/ &&
+{
+  echo ""
+  echo"# Load nvidia-settings"
+  echo "nvidia-settings -l &"
+} >> ~/.xprofile &&
+cp ~/dot-files/nvidia-install.sh ~/ &&
 chmod +x ~/nvidia-install.sh
 
 # Disable KWallet
@@ -25,4 +29,4 @@ mkdir ~/.ssh
 chmod 700 ~/.ssh
 
 # Remove repo
-rm -Rf ~/dot-files
+rm -rf ~/dot-files
