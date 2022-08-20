cfcb23435684dc68cbe3697bb1ae52566686e2a6
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-20 15:59:37 +0200

    Create plugins.lua

---
+++ plugins.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+ -- Gruvbox
+  use { "ellisonleao/gruvbox.nvim" }
+end)
