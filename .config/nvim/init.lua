98c86b4f7e735782c5ef2d71dcbfe9de63934f10
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-21 01:37:56 +0200

    Add setup for gruvbox

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,6 +39,20 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.background = 'dark'
 vim.o.termguicolors = true
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true,
+  contrast = "",
+  overrides = {},
+})
 vim.cmd([[colorscheme gruvbox]])
 
 -- Set completeopt to have a better completion experience
