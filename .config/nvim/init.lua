10e0f9b49fa8dcfc41458fad04ff17a21ee24fdf
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-20 23:13:08 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,7 +39,7 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.background = 'dark'
 vim.o.termguicolors = true
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd([[colorscheme gruvbox]])
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
