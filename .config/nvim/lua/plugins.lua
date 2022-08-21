b0673806c6042702519e428445a4d6d22587ee46
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-21 03:35:57 +0200

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,11 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -17,4 +25,8 @@ return require('packer').startup(function(use)
     
     -- nvim-tree
     use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' }, tag = 'nightly' }
+    
+    if packer_bootstrap then
+      require('packer').sync()
+    end
 end)
