7e4dc98b5795a84405eff9313520456eedce78de
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-21 02:00:29 +0200

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,4 +11,10 @@ return require('packer').startup(function(use)
 
     -- Gruvbox
     use { "ellisonleao/gruvbox.nvim" }
+    
+    -- nvim-web-icons
+    use { 'kyazdani42/nvim-web-devicons' }
+    
+    -- nvim-tree
+    use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' }, tag = 'nightly' }
 end)
