0b936431531608694f5e687c9ef80e1dabc7b73d
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-24 20:03:59 +0200

    Add .bash_profile and .bash_logout

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Copy dot-files
-cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.gitconfig ~/dot-files/.config ~/
+cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.bash_profile ~/dot-files/.bash_logout ~/dot-files/.gitconfig ~/dot-files/.config ~/
 chmod 744 ~/.bashrc_aliases
 chmod -R 744 ~/.config/custom-autostart
 
