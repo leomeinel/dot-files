fba1f4541c34424a9bfe7d4fb3aa874c330cf430
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-27 07:33:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ chmod 700 ~/src
 rustup default stable
 
 # Initialize nvim
-nvim --headless -c 'sleep! 10' -c 'sleep! q!'
+nvim --headless -c 'silent! sleep 10' -c 'silent! sleep q!'
 
 # Remove repo
 rm -rf ~/dot-files
