c3a0f18cce4336c78d3ebf17c3519fcc384b76e8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-29 03:25:42 +0200

    Remove nvidia

--- setup.sh
+++ setup.sh
@@ -7,16 +7,6 @@ set -e
 cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.bash_profile ~/dot-files/.bash_logout ~/dot-files/.gitconfig ~/dot-files/.config ~/
 chmod 744 ~/.bashrc_aliases
 
-# Copy nvidia-install.sh if required
-pacman -Qq "nvidia-settings" &&
-{
-  echo ""
-  echo "# Load nvidia-settings"
-  echo "nvidia-settings -l &"
-} > ~/.bash_profile &&
-cp ~/dot-files/nvidia-install.sh ~/ &&
-chmod +x ~/nvidia-install.sh
-
 # Create .ssh
 mkdir ~/.ssh
 chmod 700 ~/.ssh
