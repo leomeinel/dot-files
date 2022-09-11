1141436dea235853863c485031b56c5f4b37783b
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-11 17:09:58 +0200

    Don't fail on error

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Fail on error
-set -e
-
 # Copy dot-files
 cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.bash_profile ~/dot-files/.bash_logout ~/dot-files/.gitconfig ~/dot-files/.config ~/
 chmod 744 ~/.bashrc_aliases
