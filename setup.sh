224f7a545943da922f26038e6ee13bd3604cf5be
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-11 11:58:27 +0200

    Install vscodium-extensions automatically

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Copy dot-files
-cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.bash_profile ~/dot-files/.bash_logout ~/dot-files/.gitconfig ~/dot-files/.config ~/dot-files/vscodium-extensions.txt ~/
+cp -R ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.bash_profile ~/dot-files/.bash_logout ~/dot-files/.gitconfig ~/dot-files/.config ~/
 chmod 744 ~/.bashrc_aliases
 
 # Create .ssh
@@ -17,6 +17,9 @@ chmod 700 ~/src
 
 # Set default rust
 rustup default stable
+
+# Install vscodium extensions
+xargs -n 1 vscodium --install-extension < ~/dot-files/vscodium-extensions.txt
 
 # Initialize nvim
 nvim --headless -c 'sleep 10' -c 'q!'
