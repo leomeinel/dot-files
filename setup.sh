fff51e8e407c5d67fbd2831d1e9036ecc017b3ba
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-26 21:10:15 +0200

    Dont't chmod ~/.bashrc_aliases

--- setup.sh
+++ setup.sh
@@ -14,7 +14,6 @@ set -e
 
 # Copy dot-files
 cp -R ~/dot-files/.config ~/dot-files/.bash_logout ~/dot-files/.bash_profile ~/dot-files/.bashrc ~/dot-files/.bashrc_aliases ~/dot-files/.gtkrc-2.0 ~/
-chmod 744 ~/.bashrc_aliases
 source ~/.bash_profile
 
 # Create .ssh
