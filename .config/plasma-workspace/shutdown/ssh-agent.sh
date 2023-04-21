aa625c7c963d7748928db093c1e93bbafb9bd727
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-21 08:39:02 +0200

    Use double brackets

--- .config/plasma-workspace/shutdown/ssh-agent.sh
+++ .config/plasma-workspace/shutdown/ssh-agent.sh
@@ -3,11 +3,12 @@
 # File: ssh-agent.sh
 # Author: Leopold Meinel (leo@meinel.dev)
 # -----
-# Copyright (c) 2022 Leopold Meinel & contributors
+# Copyright (c) 2023 Leopold Meinel & contributors
 # SPDX ID: GPL-3.0-or-later
 # URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 # -----
 ###
 
 # Stop ssh-agent if it is started
-[ -n "$SSH_AUTH_SOCK" ] && eval "$(ssh-agent -k)"
+[[ -n "$SSH_AUTH_SOCK" ]] &&
+    eval "$(ssh-agent -k)"
