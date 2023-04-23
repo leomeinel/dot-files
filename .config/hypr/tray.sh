0b0f5fb6875ac28efaa35b6a7066b078e709d001
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-23 14:37:15 +0200

    Configure tray

---
+++ .config/hypr/tray.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###
+# File: tray.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2023 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+waybar &
+sleep 1
+nm-applet --indicator &
+nextcloud --background &
