7b4cf7da65cedcfc2409e41dcaabc7db7c6a0330
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-02 00:38:32 +0200

    Add fixes described by nvim-tree.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Plugins
 require('plugins')
 require('setup')
@@ -31,6 +35,7 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.background = 'dark'
 vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
