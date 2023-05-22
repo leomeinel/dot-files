9fcf1f04e26b37fc9c799d42e97a25764a2713a0
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-22 03:21:44 +0200

    Simplify vim init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -36,7 +36,6 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.background = 'dark'
 vim.o.termguicolors = true
-vim.opt.termguicolors = true
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -45,34 +44,22 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.clipboard = 'unnamedplus'
 
 -- Wrapping
-vim.wo.cursorline = true
-vim.wo.linebreak = true
 vim.wo.wrap = false
-vim.wo.foldmethod = 'marker'
-vim.bo.modeline = true
-vim.o.modeline = true
+
+-- Cursor line
+vim.wo.cursorline = true
 
 -- Tab
-vim.bo.tabstop = 4
 vim.o.tabstop = 4
-
-vim.bo.shiftwidth = 4
 vim.o.shiftwidth = 4
-
-vim.bo.softtabstop = 4
 vim.o.softtabstop = 4
-
-vim.bo.expandtab = true
 vim.o.expandtab = true
-
 vim.o.smarttab = true
 
 -- Indentation
-vim.bo.autoindent = true
 vim.o.autoindent = true
 
 -- Swapfile
-vim.bo.swapfile = false
 vim.o.swapfile = false
 
 -- Encoding
