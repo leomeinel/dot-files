d57773652db0cecf95617cbd11103bedcd686f92
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-04 19:02:27 +0100

    Use parallel with --retries 10 instead of xargs -n 1

--- exts-code.sh
+++ exts-code.sh
@@ -3,7 +3,7 @@
 # File: exts-code.sh
 # Author: Leopold Meinel (leo@meinel.dev)
 # -----
-# Copyright (c) 2023 Leopold Meinel & contributors
+# Copyright (c) 2025 Leopold Meinel & contributors
 # SPDX ID: GPL-3.0-or-later
 # URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 # -----
@@ -11,7 +11,7 @@
 
 # Install vscodium extensions
 vscodium --list-extensions | xargs -n 1 vscodium --uninstall-extension
-xargs -n 1 vscodium --install-extension <~/dot-files/exts-code.txt
+parallel --retries 10 vscodium --install-extension <~/dot-files/exts-code.txt
 
 # Fail on error
 set -e
