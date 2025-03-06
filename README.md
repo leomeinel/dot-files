1d90a0f8ea3b86bac25b1365381fe574c2dba3ee
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-06 19:57:03 +0100

    Add update script

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ Configuration files and miscellaneous files for my personal linux setups
 Edit `install.conf` to specify the usernames matching your system. If any of the users are not needed by you, you can just leave them how they are.
 
 ```sh
-git clone https://github.com/leomeinel/dot-files.git ~/.config/home-manager
-chmod +x ~/.config/home-manager/set-variables.sh
-~/.config/home-manager/set-variables.sh
-chmod +x ~/.config/home-manager/setup.sh
-~/.config/home-manager/setup.sh $USER
+git clone https://github.com/leomeinel/dot-files.git ~/.config/dot-files
+chmod +x ~/.config/dot-files/setup.sh
+~/.config/dot-files/setup.sh $USER
+```
+
+## Updating
+
+```sh
+git clone https://github.com/leomeinel/dot-files.git ~/.config/dot-files
+chmod +x ~/.config/dot-files/update.sh
+~/.config/dot-files/update.sh $USER
 ```
---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+###
+# File: update.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2025 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
+# Source config
+SCRIPT_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+source "$SCRIPT_DIR/install.conf"
+
+# Fail on error
+set -e
+
+if [[ -n "${1}" ]] >/dev/null 2>&1; then
+    # Remove certain codium configs for home-manager to be able to regenerate them
+    rm -rf ~/.vscode-oss ~/.vscode-R ~/.config/vscode-sqltools ~/.local/share/vscode-sqltools
+    # Run home-manager
+    nix run home-manager/release-"$NIX_VERSION" switch -b "bak" --flake "$SCRIPT_DIR/#$1"
+    git add .
+    git commit -m "Install dot-files - $DATE"
+else
+    echo "ERROR: You have to use a user from $SCRIPT_DIR/flake.nix as first argument"
+    exit 1
+fi
