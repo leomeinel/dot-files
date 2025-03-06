63fdc75fc9e87b8bdf818a5b3920c8a350becc1c
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-06 03:10:51 +0100

    Use nix run to initialize home-manager
    This means that one doesn't have to install home-manager manually

--- setup.sh
+++ setup.sh
@@ -10,11 +10,13 @@
 ###
 # Source config
 SCRIPT_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+source "$SCRIPT_DIR/install.conf"
 
 # Fail on error
 set -e
 
 if [[ -n "${1}" ]] >/dev/null 2>&1; then
+    nix run home-manager/relase-"$NIX_VERSION" -- init --switch
     home-manager switch -b "bak" --flake "$SCRIPT_DIR/#$1"
 else
     echo "ERROR: You have to use a user from $SCRIPT_DIR/flake.nix as first argument"
