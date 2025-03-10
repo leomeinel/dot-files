6a934982bacd9f6e4e906f5226048f3057b3a2ca
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-10 23:29:44 +0100

    Update lock file when updating

--- update.sh
+++ update.sh
@@ -18,9 +18,11 @@ set -e
 
 # Remove codium configs for home-manager to be able to regenerate them
 rm -rf ~/.vscode-oss ~/.vscode-R ~/.config/vscode-sqltools ~/.local/share/vscode-sqltools
+# Update lock file
+cd "$SCRIPT_DIR"
+nix flake update --commit-lock-file
 # Run home-manager
 nix run home-manager/release-"$NIX_VERSION" -- switch -b "bak" --flake "$SCRIPT_DIR/#$USER"
 DATE="$(/usr/bin/date +"%F-%H")"
-cd "$SCRIPT_DIR"
 git add .
 git commit -m "Install dot-files - $DATE"
