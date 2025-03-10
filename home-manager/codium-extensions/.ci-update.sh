bc863b698020af8da4567a37188affe5d1c0febb
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-10 22:38:56 +0100

    Add gitlab-ci.yml

--- home-manager/codium-extensions/.ci-update.sh
+++ home-manager/codium-extensions/.ci-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ###
-# File: update.sh
+# File: .ci-update.sh
 # Author: Leopold Meinel (leo@meinel.dev)
 # -----
 # Copyright (c) 2025 Leopold Meinel & contributors
@@ -9,11 +9,9 @@
 # -----
 ###
 
-# FIXME: This should be a CI/CD pipeline.
-#        TODO: Configure build environemnt, Get latest nix CODIUM_VERSION dynamically, ...
-
 # Source config
 SCRIPT_DIR="$(dirname -- "$(readlink -f -- "$0")")"
+source "$SCRIPT_DIR/../../../install.conf"
 
 # Fail on error
 set -e
@@ -26,7 +24,7 @@ sed_exit() {
 }
 
 # Set current version of codium
-CODIUM_VERSION="$(codium -v | head -n 1)"
+CODIUM_VERSION="$(nix run nixpkgs#nix-search-cli -- -c $NIX_VERSION -n 'vscodium' | grep 'vscodium @' | awk '{print $3}' | awk 'BEGIN{FS=OFS="."}{$NF=""; NF--; print}')"
 if [[ -n "${CODIUM_VERSION}" ]] >/dev/null 2>&1; then
     ## START sed
     FILE="$SCRIPT_DIR"/config.toml
@@ -44,4 +42,8 @@ cd "$SCRIPT_DIR"/nix4vscode
 git submodule update --remote
 
 # Generate codium-extensions.nix dynamically
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
 nix develop --command bash -c "cargo run -q -- $SCRIPT_DIR/config.toml" >"$SCRIPT_DIR"/codium-extensions.nix
