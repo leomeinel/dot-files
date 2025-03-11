9a95b7ddb362b5639a29ed212f36804f333cf4b4
project_21_bot_8dd50112a6fa66541c08737f663af158 <project_21_bot_8dd50112a6fa66541c08737f663af158@noreply.git.ber-m-leo.cyan.home.arpa>
2025-03-11 12:29:59 +0000

    Regen codium-extensions.nix - CI 2025-03-11-12

--- home-manager/codium-extensions/codium-extensions.nix
+++ home-manager/codium-extensions/codium-extensions.nix
@@ -11,39 +11,11 @@ merge
     (merge
       (merge
       {
-        "kisstkondoros"."vscode-gutter-preview" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-gutter-preview";
-          publisher = "kisstkondoros";
-          version = "0.32.2";
-          sha256 = "1b8rp41j99li2718599lrvr8y49h8vlz20vzl1wxdjxhdd8gi2i4";
-
-        };
-        "franneck94"."workspace-formatter" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "workspace-formatter";
-          publisher = "franneck94";
-          version = "1.1.0";
-          sha256 = "0rliczyvv7c3bpy1drksm31gha2sqp7x3v6v83y9gz2jq190m9bx";
-
-        };
-        "htmlhint"."vscode-htmlhint" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-htmlhint";
-          publisher = "htmlhint";
-          version = "1.0.5";
-          sha256 = "079rpiy0fxhzsd6cli4nxbl3r9kmw98wcjmssm304ab1f4ghlj1c";
-
-        };
-        "kokororin"."vscode-phpfmt" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-phpfmt";
-          publisher = "kokororin";
-          version = "1.2.41";
-          sha256 = "033y599slnw73p8c23i4nxfn9rngi4gdr0mb0xr7vlc2ba5f0r4q";
-
-        };
-        "franneck94"."vscode-c-cpp-config" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-c-cpp-config";
-          publisher = "franneck94";
-          version = "6.3.0";
-          sha256 = "1js5bw7aas9j3c1iaa7rh5vjfpyxxqgdpq5583xn9l1hgjvmkg50";
+        "mtxr"."sqltools-driver-pg" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "sqltools-driver-pg";
+          publisher = "mtxr";
+          version = "0.5.5";
+          sha256 = "0fwjkn0aicfv9b5y1d7pjxpz9mdchs7ygidiyf8gcn2j65q34p07";
 
         };
         "mkhl"."shfmt" = vscode-utils.extensionFromVscodeMarketplace {
@@ -53,25 +25,46 @@ merge
           sha256 = "0zp1lhx0l9nyrpsdws6jmrjqi7bkdj3mzx7l1d5l56cv0qzmgfjp";
 
         };
-        "njqdev"."vscode-python-typehint" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-python-typehint";
-          publisher = "njqdev";
-          version = "1.5.1";
-          sha256 = "0ra3jg8xwa8vssz33cgfkjbn4chsir1xlzshij0jn37zmw42q8q8";
+        "franneck94"."workspace-formatter" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "workspace-formatter";
+          publisher = "franneck94";
+          version = "1.1.0";
+          sha256 = "0rliczyvv7c3bpy1drksm31gha2sqp7x3v6v83y9gz2jq190m9bx";
 
         };
-        "13xforever"."language-x86-64-assembly" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "language-x86-64-assembly";
-          publisher = "13xforever";
-          version = "3.1.4";
-          sha256 = "1kvy9kxgnm6r9973zyk65z5w2a54dh2iclm091gv067pa6dl750l";
+        "pranaygp"."vscode-css-peek" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-css-peek";
+          publisher = "pranaygp";
+          version = "4.4.3";
+          sha256 = "05b2xrrhjss7icm2w0xqb6j5wbqy1lrxh92ihjwkqfgn7fjad3x1";
 
         };
-        "jeff-hykin"."better-cpp-syntax" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "better-cpp-syntax";
-          publisher = "jeff-hykin";
-          version = "1.27.1";
-          sha256 = "037nigza7n71j5vgl3qw0swrmy8gvfj9m8jbi1nb2a3lmsifivqq";
+        "franneck94"."vscode-python-config" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-python-config";
+          publisher = "franneck94";
+          version = "12.4.0";
+          sha256 = "1dpsjqilijlj1i66721xla6h9979vibgms0ryr71nyga5yazq7s1";
+
+        };
+        "mtxr"."sqltools-driver-mssql" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "sqltools-driver-mssql";
+          publisher = "mtxr";
+          version = "0.4.4";
+          sha256 = "1fznfdc2mk7g8pafdw3vzls69l0qv2wgcrmzxghyj5bsf06ig666";
+
+        };
+        "kisstkondoros"."vscode-gutter-preview" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-gutter-preview";
+          publisher = "kisstkondoros";
+          version = "0.32.2";
+          sha256 = "1b8rp41j99li2718599lrvr8y49h8vlz20vzl1wxdjxhdd8gi2i4";
+
+        };
+        "franneck94"."vscode-c-cpp-dev-extension-pack" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-c-cpp-dev-extension-pack";
+          publisher = "franneck94";
+          version = "0.10.0";
+          sha256 = "0dqyr8kyj6qj34x34x078b5xflhlzvi1k7ld3hzm1mf9l5y2mdgg";
 
         };
         "mtxr"."sqltools" = vscode-utils.extensionFromVscodeMarketplace {
@@ -79,6 +72,69 @@ merge
           publisher = "mtxr";
           version = "0.28.4";
           sha256 = "0bszagbm10004rx2jdhg2g33wg0f1l0kp3cs1jzkdj1r4an9w5qk";
+
+        };
+        "psioniq"."psi-header" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "psi-header";
+          publisher = "psioniq";
+          version = "1.25.1";
+          sha256 = "0hsx8nghwcijdydy2wd16g4a1gz1d7lbyxszk1rz6k7wa36wzgya";
+
+        };
+        "bbenoist"."doxygen" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "doxygen";
+          publisher = "bbenoist";
+          version = "1.0.0";
+          sha256 = "0kclb60mnaj3c5lmpwmhkbnx4g8gr4wy66lkcklkwm555nkgw48n";
+
+        };
+        "swellaby"."vscode-rust-test-adapter" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-rust-test-adapter";
+          publisher = "swellaby";
+          version = "0.11.0";
+          sha256 = "111vhl71zzh4il1kh21l49alwlllzcvmdbsxyvk9bq3r24hxq1r2";
+
+        };
+        "franneck94"."c-cpp-runner" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "c-cpp-runner";
+          publisher = "franneck94";
+          version = "9.4.10";
+          sha256 = "187rybxj8i2flnljac2fhcbb6mpk4i33j9293ph8zzdv5i2y21wh";
+
+        };
+        "franneck94"."vscode-python-dev-extension-pack" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-python-dev-extension-pack";
+          publisher = "franneck94";
+          version = "2.0.0";
+          sha256 = "0icws51p3kzvi0wd95db9infvjfdgklrn1nyv9qw6zr6scdmzwdm";
+
+        };
+        "zignd"."html-css-class-completion" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "html-css-class-completion";
+          publisher = "zignd";
+          version = "1.20.0";
+          sha256 = "1hc2dgib3wryygb36h47wzf32iv1x6rn1swmbgchiyjw62jjj4fw";
+
+        };
+        "franneck94"."vscode-c-cpp-config" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-c-cpp-config";
+          publisher = "franneck94";
+          version = "6.3.0";
+          sha256 = "1js5bw7aas9j3c1iaa7rh5vjfpyxxqgdpq5583xn9l1hgjvmkg50";
+
+        };
+        "ms-vscode"."cpptools-themes" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "cpptools-themes";
+          publisher = "ms-vscode";
+          version = "2.0.0";
+          sha256 = "05r7hfphhlns2i7zdplzrad2224vdkgzb0dbxg40nwiyq193jq31";
+
+        };
+        "cheshirekow"."cmake-format" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "cmake-format";
+          publisher = "cheshirekow";
+          version = "0.6.11";
+          sha256 = "14v0wb00iy38ry9bfpzz4fjraggy4ygg5v622mfpxb7498kkrm9m";
 
         };
         "bierner"."color-info" = vscode-utils.extensionFromVscodeMarketplace {
@@ -95,46 +151,25 @@ merge
           sha256 = "1cwncjvarq3g0cmn9afdd15s81nms0kcawnj3midhr4hchap2aml";
 
         };
-        "franneck94"."vscode-c-cpp-dev-extension-pack" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-c-cpp-dev-extension-pack";
-          publisher = "franneck94";
-          version = "0.10.0";
-          sha256 = "0dqyr8kyj6qj34x34x078b5xflhlzvi1k7ld3hzm1mf9l5y2mdgg";
-
-        };
-        "bbenoist"."doxygen" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "doxygen";
-          publisher = "bbenoist";
-          version = "1.0.0";
-          sha256 = "0kclb60mnaj3c5lmpwmhkbnx4g8gr4wy66lkcklkwm555nkgw48n";
-
-        };
-        "psioniq"."psi-header" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "psi-header";
-          publisher = "psioniq";
-          version = "1.25.1";
-          sha256 = "0hsx8nghwcijdydy2wd16g4a1gz1d7lbyxszk1rz6k7wa36wzgya";
-
-        };
-        "pranaygp"."vscode-css-peek" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-css-peek";
-          publisher = "pranaygp";
-          version = "4.4.3";
-          sha256 = "05b2xrrhjss7icm2w0xqb6j5wbqy1lrxh92ihjwkqfgn7fjad3x1";
-
-        };
-        "mtxr"."sqltools-driver-pg" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "sqltools-driver-pg";
-          publisher = "mtxr";
+        "rdebugger"."r-debugger" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "r-debugger";
+          publisher = "rdebugger";
           version = "0.5.5";
-          sha256 = "0fwjkn0aicfv9b5y1d7pjxpz9mdchs7ygidiyf8gcn2j65q34p07";
+          sha256 = "0v45fbcpqgdlql4splslvccccrrsnrcxs1qqcx5bv7a0r6v9l7s5";
 
         };
-        "zignd"."html-css-class-completion" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "html-css-class-completion";
-          publisher = "zignd";
-          version = "1.20.0";
-          sha256 = "1hc2dgib3wryygb36h47wzf32iv1x6rn1swmbgchiyjw62jjj4fw";
+        "htmlhint"."vscode-htmlhint" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-htmlhint";
+          publisher = "htmlhint";
+          version = "1.0.5";
+          sha256 = "079rpiy0fxhzsd6cli4nxbl3r9kmw98wcjmssm304ab1f4ghlj1c";
+
+        };
+        "mtxr"."sqltools-driver-mysql" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "sqltools-driver-mysql";
+          publisher = "mtxr";
+          version = "0.6.4";
+          sha256 = "0fw4w5pfw91s8lbam1k81g9vwjgrpn93dd05lr7r9rgzdw102fcf";
 
         };
         "hangxingliu"."vscode-nginx-conf-hint" = vscode-utils.extensionFromVscodeMarketplace {
@@ -151,32 +186,32 @@ merge
           sha256 = "1d95znf2vsdzv9jqiigh9zm62dp4m9jz3qcfaxn0n0pvalbiyw92";
 
         };
-        "gruntfuggly"."activitusbar" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "activitusbar";
-          publisher = "gruntfuggly";
-          version = "0.0.47";
-          sha256 = "16ianj0cdnzmqnq3jnx5nd7yd9w0fmrzwdn3d2a1r7a5r05y9f44";
+        "jeff-hykin"."better-cpp-syntax" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "better-cpp-syntax";
+          publisher = "jeff-hykin";
+          version = "1.27.1";
+          sha256 = "037nigza7n71j5vgl3qw0swrmy8gvfj9m8jbi1nb2a3lmsifivqq";
 
         };
-        "mtxr"."sqltools-driver-mysql" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "sqltools-driver-mysql";
-          publisher = "mtxr";
-          version = "0.6.4";
-          sha256 = "0fw4w5pfw91s8lbam1k81g9vwjgrpn93dd05lr7r9rgzdw102fcf";
+        "kokororin"."vscode-phpfmt" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-phpfmt";
+          publisher = "kokororin";
+          version = "1.2.41";
+          sha256 = "033y599slnw73p8c23i4nxfn9rngi4gdr0mb0xr7vlc2ba5f0r4q";
 
         };
-        "cheshirekow"."cmake-format" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "cmake-format";
-          publisher = "cheshirekow";
-          version = "0.6.11";
-          sha256 = "14v0wb00iy38ry9bfpzz4fjraggy4ygg5v622mfpxb7498kkrm9m";
+        "njqdev"."vscode-python-typehint" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "vscode-python-typehint";
+          publisher = "njqdev";
+          version = "1.5.1";
+          sha256 = "0ra3jg8xwa8vssz33cgfkjbn4chsir1xlzshij0jn37zmw42q8q8";
 
         };
-        "franneck94"."c-cpp-runner" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "c-cpp-runner";
-          publisher = "franneck94";
-          version = "9.4.10";
-          sha256 = "187rybxj8i2flnljac2fhcbb6mpk4i33j9293ph8zzdv5i2y21wh";
+        "13xforever"."language-x86-64-assembly" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "language-x86-64-assembly";
+          publisher = "13xforever";
+          version = "3.1.4";
+          sha256 = "1kvy9kxgnm6r9973zyk65z5w2a54dh2iclm091gv067pa6dl750l";
 
         };
         "mtxr"."sqltools-driver-sqlite" = vscode-utils.extensionFromVscodeMarketplace {
@@ -186,34 +221,6 @@ merge
           sha256 = "0rfs00s3im85b3ip1af1jv4qf33dcx8wzr9nlfl08sbjb4b20gk9";
 
         };
-        "rdebugger"."r-debugger" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "r-debugger";
-          publisher = "rdebugger";
-          version = "0.5.5";
-          sha256 = "0v45fbcpqgdlql4splslvccccrrsnrcxs1qqcx5bv7a0r6v9l7s5";
-
-        };
-        "mtxr"."sqltools-driver-mssql" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "sqltools-driver-mssql";
-          publisher = "mtxr";
-          version = "0.4.4";
-          sha256 = "1fznfdc2mk7g8pafdw3vzls69l0qv2wgcrmzxghyj5bsf06ig666";
-
-        };
-        "franneck94"."vscode-python-config" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-python-config";
-          publisher = "franneck94";
-          version = "12.4.0";
-          sha256 = "1dpsjqilijlj1i66721xla6h9979vibgms0ryr71nyga5yazq7s1";
-
-        };
-        "swellaby"."vscode-rust-test-adapter" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-rust-test-adapter";
-          publisher = "swellaby";
-          version = "0.11.0";
-          sha256 = "111vhl71zzh4il1kh21l49alwlllzcvmdbsxyvk9bq3r24hxq1r2";
-
-        };
         "ms-python"."mypy-type-checker" = vscode-utils.extensionFromVscodeMarketplace {
           name = "mypy-type-checker";
           publisher = "ms-python";
@@ -221,18 +228,11 @@ merge
           sha256 = "125vr6irqn5q78ydgrql3lfkwjn264amzv52qkh8hv0w5r4dg7sl";
 
         };
-        "ms-vscode"."cpptools-themes" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "cpptools-themes";
-          publisher = "ms-vscode";
-          version = "2.0.0";
-          sha256 = "05r7hfphhlns2i7zdplzrad2224vdkgzb0dbxg40nwiyq193jq31";
-
-        };
-        "franneck94"."vscode-python-dev-extension-pack" = vscode-utils.extensionFromVscodeMarketplace {
-          name = "vscode-python-dev-extension-pack";
-          publisher = "franneck94";
-          version = "2.0.0";
-          sha256 = "0icws51p3kzvi0wd95db9infvjfdgklrn1nyv9qw6zr6scdmzwdm";
+        "gruntfuggly"."activitusbar" = vscode-utils.extensionFromVscodeMarketplace {
+          name = "activitusbar";
+          publisher = "gruntfuggly";
+          version = "0.0.47";
+          sha256 = "16ianj0cdnzmqnq3jnx5nd7yd9w0fmrzwdn3d2a1r7a5r05y9f44";
 
         };
       }
@@ -240,4 +240,3 @@ merge
       (lib.attrsets.optionalAttrs (isLinux && (isAarch32 || isAarch64)) { }))
     (lib.attrsets.optionalAttrs (isDarwin && (isi686 || isx86_64)) { }))
   (lib.attrsets.optionalAttrs (isDarwin && (isAarch32 || isAarch64)) { })
-
