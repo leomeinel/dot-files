ed5c6607ce519aa35816c0717cc1ecc7b8bd00e2
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-21 05:40:57 +0100

    Add workaround for GTK4 flatpaks

--- home-manager/common-home-gui.nix
+++ home-manager/common-home-gui.nix
@@ -62,10 +62,17 @@
         # Delete wofi cache to prevent saving deprecated symlinks
         run rm -f ${config.xdg.cacheHome}/wofi-run
 
+        # FIXME: This shouldn't be necessary
+        # Reset flatpak override to make sure that no unnecessary overrides are added
+        run flatpak override -u --reset
         # Workaround for using the correct font in all applications
-        ln -sfn ${config.xdg.stateHome}/nix/profile/share/fonts ${config.xdg.dataHome}/fonts
+        run ln -sfn ${config.xdg.stateHome}/nix/profile/share/fonts ${config.xdg.dataHome}/fonts
+        run flatpak override -u --filesystem=${config.xdg.dataHome}/fonts:ro --filesystem=${config.xdg.stateHome}/nix/profile/share/fonts:ro --filesystem=${pkgs.hasklig}/share/fonts:ro --filesystem=${pkgs.noto-fonts}/share/fonts:ro
+        # Workaround for using the correct icons in all applications
+        run flatpak override -u --filesystem=${config.xdg.dataHome}/icons:ro --filesystem=${config.xdg.stateHome}/nix/profile/share/icons:ro --filesystem=${pkgs.papirus-icon-theme}/share/icons:ro --filesystem=${pkgs.phinger-cursors}/share/icons:ro
         # Workaround for using the correct GTK theme in all applications
-        ln -sfn ${config.xdg.stateHome}/nix/profile/share/themes ${config.xdg.dataHome}/themes
+        run ln -sfn ${config.xdg.stateHome}/nix/profile/share/themes ${config.xdg.dataHome}/themes
+        run flatpak override -u --filesystem=${config.xdg.dataHome}/themes:ro --filesystem=${config.xdg.stateHome}/nix/profile/share/themes:ro --filesystem=${pkgs.arc-theme}/share/themes:ro
       '';
     };
   };
