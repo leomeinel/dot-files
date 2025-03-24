69476a2be3b861e3d01d3f6f709eb4d5cd9d2b8d
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-24 19:36:17 +0100

    Include Renviron
    I'm pretty sure that this was not the problem. If it was, that would've probably been mentioned in the nix documentation. (I at least hope so)

--- home-manager/configs/home/xdg-gui.nix
+++ home-manager/configs/home/xdg-gui.nix
@@ -38,6 +38,9 @@
         source = ../../files/.config/nwg-bar/style.css;
       };
       # r
+      "r/Renviron" = {
+        source = ../../files/.config/r/Renviron;
+      };
       "r/Rprofile" = {
         source = ../../files/.config/r/Rprofile;
       };
