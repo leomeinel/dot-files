b4b24a65b0074ec495c4f3da9a5c11b213cbd4ea
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-27 06:45:02 +0100

    Capitalize variable

--- home-manager/files/.config/sway/scripts/volume.sh
+++ home-manager/files/.config/sway/scripts/volume.sh
@@ -12,18 +12,18 @@
 CURRENT="$(/usr/bin/pactl get-sink-volume @DEFAULT_SINK@ | /usr/bin/grep -e '%' | /usr/bin/sed 's/ //g' | /usr/bin/cut -d "/" -f 2 | /usr/bin/sed 's/%//')"
 
 up() {
-    new="$((CURRENT + 5))"
-    if [[ "${new}" -lt 100 ]]; then
-        /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ "${new}"%
+    NEW="$((CURRENT + 5))"
+    if [[ "${NEW}" -lt 100 ]]; then
+        /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ "${NEW}"%
     else
         /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ 100%
     fi
 }
 
 down() {
-    new="$((CURRENT - 5))"
-    if [[ "${new}" -gt 0 ]]; then
-        /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ "${new}"%
+    NEW="$((CURRENT - 5))"
+    if [[ "${NEW}" -gt 0 ]]; then
+        /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ "${NEW}"%
     else
         /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ 0%
     fi
