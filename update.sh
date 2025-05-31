c470f8c670d70ceb6a5d06bfc76c8bc1ca35699d
Leopold Johannes Meinel <leo@meinel.dev>
2025-05-31 12:53:55 +0200

    Use nix-shell -p home-manager

--- update.sh
+++ update.sh
@@ -24,8 +24,14 @@ rm -rf ~/.vscode-oss ~/.vscode-R ~/.config/vscode-sqltools ~/.local/share/vscode
 # Update lock file
 cd "${SCRIPT_DIR}"
 nix flake update
+
+# Add nixpkgs channel
+nix-channel --add https://nixos.org/channels/nixos-"${NIX_VERSION}" nixpkgs || true
+nix-channel --update
+
 # Run home-manager
-nix run home-manager/release-"${NIX_VERSION}" -- switch -b "bak" --flake "${SCRIPT_DIR}/#${USER}"
+nix-shell -p home-manager
+home-manager switch -b "bak" --flake "${SCRIPT_DIR}/#${USER}"
 
 # Source ~/.bash_profile
 # shellcheck source=/dev/null
@@ -39,10 +45,6 @@ git commit --no-gpg-sign -m "Update dot-files - ${DATE}" || true
 # Set default rust if rustup is installed
 [[ -n "$(which rustup)" ]] >/dev/null 2>&1 &&
     rustup default stable
-
-# Add nixpkgs channel
-nix-channel --add https://nixos.org/channels/nixos-"${NIX_VERSION}" nixpkgs || true
-nix-channel --update
 
 # Notify user if script has finished successfully
 echo "'$(basename "${0}")' has finished successfully."
