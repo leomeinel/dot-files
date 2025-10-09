df1eb138155c0d28dcc536015bd097f68e5e1431
Leopold Johannes Meinel <leo@meinel.dev>
2025-10-09 15:50:04 +0200

    Fix = in header

--- home-manager/configs/home/fonts-gui.nix
+++ home-manager/configs/home/fonts-gui.nix
@@ -1,10 +1,10 @@
 /*
-  File = fonts-gui.nix
-  Author = Leopold Meinel (leo@meinel.dev)
+  File: fonts-gui.nix
+  Author: Leopold Meinel (leo@meinel.dev)
   -----
   Copyright (c) 2025 Leopold Meinel & contributors
-  SPDX ID = MIT
-  URL = https://opensource.org/licenses/MIT
+  SPDX ID: MIT
+  URL: https://opensource.org/licenses/MIT
   -----
 */
 
