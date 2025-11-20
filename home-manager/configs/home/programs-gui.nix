e8ca367e6390cd469d26850855ce96ba27f583a3
Leopold Johannes Meinel <leo@meinel.dev>
2025-11-20 05:26:29 +0100

    License: Update headers

--- home-manager/configs/home/programs-gui.nix
+++ home-manager/configs/home/programs-gui.nix
@@ -3,9 +3,8 @@
   Author: Leopold Johannes Meinel (leo@meinel.dev)
   -----
   Copyright (c) 2025 Leopold Johannes Meinel & contributors
-  SPDX ID: MIT
-  URL: https://opensource.org/licenses/MIT
-  -----
+  SPDX ID: Apache-2.0
+  URL: https://www.apache.org/licenses/LICENSE-2.0
 */
 
 {
